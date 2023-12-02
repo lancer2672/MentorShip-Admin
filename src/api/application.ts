b2819701bdd5cc4ca33858e999024314c378f308
lancer2672 <jbkhanhtran@gmail.com>
2023-12-02 20:03:41 +0700

    update application api

--- src/api/application.ts
+++ src/api/application.ts
@@ -1,6 +1,56 @@
 import axiosClient from "../config/axios-client";
 
 const applicationApi = {
-  getAll: () => {},
-  updateApplicationStatus: () => {},
+  getAll: async () => {
+    try {
+      const url = "/application/getAllApplication";
+      const res = await axiosClient.get(url);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
+  getApplicationById: async (id) => {
+    try {
+      const url = `/application/getApplicationById/${id}`;
+      const res = await axiosClient.get(url);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
+  updateApplicationStatus: async (id, status) => {
+    try {
+      const url = `/application/updateApplicationStatus/${id}`;
+      const res = await axiosClient.put(url, status);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
+  deleteApplication: async (id) => {
+    try {
+      const url = `/application/deleteApplication/${id}`;
+      const res = await axiosClient.delete(url);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
+  createApplication: async (application) => {
+    try {
+      const url = "/application/createApplication";
+      const res = await axiosClient.post(url, application);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
 };
+
+export default applicationApi;
