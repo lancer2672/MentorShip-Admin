a1e736ada6e3a7d3acd97f551e48e439e5f59716
lancer2672 <jbkhanhtran@gmail.com>
2023-12-02 20:08:37 +0700

    update user store

---
+++ src/store/userStore.ts
@@ -0,0 +1,23 @@
+import create from "zustand";
+
+type User = {
+  id: string;
+  displayName: string;
+  email: string;
+  phoneNumber: string;
+};
+
+type UserState = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+export const useUserStore = create<UserState>((set) => ({
+  user: {
+    id: "6545fadf162346b2815226b5",
+    displayName: "Default User",
+    email: "default.user@example.com",
+    phoneNumber: "123-456-7890",
+  },
+  setUser: (user) => set({ user }),
+}));
